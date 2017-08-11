0b46865ae4bd375e75e6cd5aef335c339e6f7521
Mounika <mounika.tunga9@gmail.com>
2017-08-11 11:35:38 -0700

    confirm

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,17 +3,47 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using System.Linq;
+using restauranter.Models;
 
 namespace restauranter.Controllers
 {
     public class HomeController : Controller
     {
+        private RestaurantContext _context;
+ 
+        public HomeController(RestaurantContext context)
+        {
+            _context = context;
+        }
         // GET: /Home/
         [HttpGet]
         [Route("")]
         public IActionResult Index()
         {
+            
             return View();
         }
+        [HttpGet]
+        [Route("list")]
+        public IActionResult List()
+        {
+            List<Person> AllUsers = _context.user.ToList();
+            ViewBag.AllUsers = AllUsers;
+            return View("list");
+        }
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Create(Person NewPerson)
+        {
+            NewPerson.created_at = DateTime.Now;
+            NewPerson.updated_at = DateTime.Now;
+
+             _context.Add(NewPerson);
+            // OR _context.Users.Add(NewPerson);
+            _context.SaveChanges();
+            return RedirectToAction("List");
+        }
+
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -4,6 +4,7 @@ using System;
 namespace restauranter.Models{
     
     public class Person{
+        [Key]
         public int iduser {get;set;}
 
         [Required]
@@ -22,9 +23,9 @@ namespace restauranter.Models{
  
         [Required]
         
-        public DateTime Visit {get;set;}
-        public DateTime CreatedAt {get;set;}
-        public DateTime UpdatedAt {get;set;}
+        public DateTime VisitDate {get;set;}
+        public DateTime created_at {get;set;}
+        public DateTime updated_at {get;set;}
         public int Stars {get; set;}
         
       
